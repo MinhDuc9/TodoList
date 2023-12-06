cdf68a353e0142885d2d3d41e9c41073b203050b
MinhDuc9 <minhduc0567@gmail.com>
2023-12-06 13:13:47 +0700

    Fixed timeData

--- src/app/controllers/TodoController.js
+++ src/app/controllers/TodoController.js
@@ -18,9 +18,21 @@ class TodoController {
 
                 arr.forEach((x) => {
                     let deadline = moment(x.dueDate);
+
+                    // Calculate the difference in milliseconds
+                    const diff = deadline.diff(currentDate, 'milliseconds');
+
+                    // Convert milliseconds to days and hours
+                    const daysLeft = Math.floor(diff / (1000 * 60 * 60 * 24));
+                    const hoursLeft = Math.floor(
+                        (diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
+                    );
+
+                    // Store the calculated values
                     x.timeData = {};
-                    x.timeData.daysLeft = deadline.diff(currentDate, 'days');
-                    x.timeData.hoursLeft = deadline.diff(currentDate, 'hours');
+                    x.timeData.daysLeft = daysLeft;
+                    x.timeData.hoursLeft = hoursLeft;
+                    x.timeData.hoursOnly = deadline.diff(currentDate, 'hours');
                 });
 
                 res.render('todo/index', {
